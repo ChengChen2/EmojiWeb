db62c543401608f65475698a3f607cebc571e84c
unknown <2041757928@qq.com>
2019-12-29 18:00:14 +0000

    histogram classify

--- img_OCR/img_test.py
+++ img_OCR/img_test.py
@@ -6,25 +6,36 @@ from matplotlib import pyplot as plt
 import cv2
 import os 
 
+img_num = 0
 #np.set_printoptions(threshold=np.inf)
 
-img_num = 0
+def img_histogram(input_img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    values = img_gray.ravel()
+    print(np.var(values))
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(img)
+    plt.subplot(1,2,2)
+    plt.hist(values, bins=256, range=[0,256])
+    plt.title('Histogram figure: '+ str(img_num))
+    plt.show()
 
-img_path = "class1"
+
+img_path = "class2"
 for filename in os.listdir(img_path):
     if filename.endswith('jpg') or filename.endswith('png'):
         img = cv2.imread(filename)
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        values = img_gray.ravel()
-        
-        plt.figure(figsize=(10,5))
-        plt.subplot(1,2,1)
-        plt.imshow(img)
-        plt.subplot(1,2,2)
-        plt.hist(values, bins=256, range=[0,256])
-        plt.title('figure: '+ str(img_num))
-        img_num += 1
-        plt.show()
+        if(img is None):
+            img = cv2.imdecode(np.fromfile(img_path + "/" + filename, dtype=np.uint8), -1)
+            if(img is None):
+                print("wrong img format")
+            else:
+                img_histogram(img)
+                img_num += 1
+        else:
+            img_histogram(img)
+            img_num += 1
     
 '''
 for i in range(img_num):
